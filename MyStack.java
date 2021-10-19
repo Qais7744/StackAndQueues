83b9ee3ea47ae4c2fdb64a343da14bb723ea4043
unknown <altamashbagwan1@gmail.com>
2021-10-19 23:04:50 +0530

    Added: Functioning of Stack and Queues use push method

---
+++ MyStack.java
@@ -0,0 +1,23 @@
+package com.bl.stackAndQueues;
+
+import com.bl.linkedList.INode;
+import com.bl.linkedList.MyLinkedList;
+import com.bl.linkedList.MyNode;
+
+public class MyStack {
+	private final MyLinkedList myLinkedList;
+	
+	public MyStack() {
+		this.myLinkedList = new MyLinkedList();
+	}
+	public void push(INode myNode) {
+		myLinkedList.add(myNode);
+	}
+	public void printStack() {
+		myLinkedList.printMyNodes();
+	}
+	public INode peak() {
+		return myLinkedList.head;
+	}
+	
+}
---
+++ MyStackTest.java
@@ -0,0 +1,24 @@
+package com.bl.stackAndQueues;
+
+import org.junit.Test;
+
+import com.bl.linkedList.INode;
+import com.bl.linkedList.MyNode;
+
+import junit.framework.Assert;
+
+public class MyStackTest {
+	@Test
+	public void given3NumberWhenAddedToLinkedListshouldBeAddedToTop() {
+		MyNode<Integer> myFirstNode = new MyNode<>(70);
+		MyNode<Integer> mySecondNode = new MyNode<>(30);
+		MyNode<Integer> myThirdNode = new MyNode<>(56);
+		MyStack myStack= new MyStack();
+		myStack.push(myFirstNode);
+		myStack.push(mySecondNode);
+		myStack.push(myThirdNode);
+		myStack.printStack();
+		INode peak = myStack.peak();
+		Assert.assertEquals(myThirdNode, peak);
+	}
+}
