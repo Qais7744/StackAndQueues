b1e1f1e1f62c2571f36d5005fa8b4bab46d5823b
unknown <altamashbagwan1@gmail.com>
2021-10-23 00:28:30 +0530

    Refactored: remove push method in MyQueue

--- MyQueueTest.java
+++ MyQueueTest.java
@@ -13,13 +13,12 @@ public class MyQueueTest {
 		MyNode<Integer> myFirstNode = new MyNode<>(56);
 		MyNode<Integer> mySecondNode = new MyNode<>(30);
 		MyNode<Integer> myThirdNode = new MyNode<>(70);
-		MyQueue myQueue= new MyQueue();
+		MyQueue myQueue = new MyQueue();
 		myQueue.enqueue(myFirstNode);
 		myQueue.enqueue(mySecondNode);
 		myQueue.enqueue(myThirdNode);
 		INode dequeue = myQueue.dequeue();
 		myQueue.printQueue();
 		Assert.assertEquals(myFirstNode, dequeue);
-		
 	}
 }
