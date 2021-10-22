5ff6e207ea382c25b8c76b655727835270948ec3
unknown <altamashbagwan1@gmail.com>
2021-10-23 00:33:41 +0530

    Refactored: remove push method

--- MyQueue.java
+++ MyQueue.java
@@ -2,27 +2,6 @@ package com.bl.queues;
 
 import com.bl.linkedList.INode;
 import com.bl.linkedList.MyLinkedList;
-<<<<<<< HEAD
-import com.bl.linkedList.MyNode;
-
-public class MyQueue {
-	private final MyLinkedList myLinkedList;
-	
-	public MyQueue() {
-		this.myLinkedList = new MyLinkedList();
-	}
-	
-	public void printQueue() {
-		myLinkedList.printMyNodes();
-	}
-	
-	public INode peak() {
-		return myLinkedList.head;
-	}
-
-	public void enqueue(INode myNode) {
-		myLinkedList.enqueue(myNode);
-=======
 
 public class MyQueue {
 
@@ -42,6 +21,5 @@ public class MyQueue {
 
 	public INode dequeue() {
 		return myLinkedList.dequeue();
->>>>>>> UC4_DnqueueMethode
 	}
 }
--- MyQueueTest.java
+++ MyQueueTest.java
@@ -1,16 +1,5 @@
 package com.bl.queues;
 
-<<<<<<< HEAD
-import org.junit.Test;
-import com.bl.linkedList.INode;
-import com.bl.linkedList.MyNode;
-import junit.framework.Assert;
-
-public class MyQueueTest {
-	
-	@Test
-	public void given3NumberWhenEnqueueToQueueshouldHavaLastAddedNode() {
-=======
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -21,7 +10,6 @@ public class MyQueueTest {
 
 	@Test
 	public void given3NumberWhenDequeueToQueueshouldHaveBeginningNode() {
->>>>>>> UC4_DnqueueMethode
 		MyNode<Integer> myFirstNode = new MyNode<>(56);
 		MyNode<Integer> mySecondNode = new MyNode<>(30);
 		MyNode<Integer> myThirdNode = new MyNode<>(70);
@@ -29,18 +17,8 @@ public class MyQueueTest {
 		myQueue.enqueue(myFirstNode);
 		myQueue.enqueue(mySecondNode);
 		myQueue.enqueue(myThirdNode);
-<<<<<<< HEAD
-		INode peak = myQueue.peak();
-		myQueue.printQueue();
-		Assert.assertEquals(myFirstNode, peak);
-=======
 		INode dequeue = myQueue.dequeue();
 		myQueue.printQueue();
 		Assert.assertEquals(myFirstNode, dequeue);
-<<<<<<< HEAD
->>>>>>> UC4_DnqueueMethode
-		
-=======
->>>>>>> UC4_DnqueueMethode
 	}
 }
